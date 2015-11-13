afae3c21dc12b7e42f84cb668ab247dfa5499a64
ScottEdg <scott.edgerly@maine.edu>
2015-11-13 09:34:01 -0500

    added a displayContents() function

--- HW#8/main.cpp
+++ HW#8/main.cpp
@@ -13,7 +13,7 @@ This code uses lists to send out trucks with orders to the correct destination.
 #include <string>
 #include <list>
 #include <ostream>
-
+void displayContents(list<Truck>::iterator it);//POST: display contents of a Truck
 using namespace std;
 int main()
 {
@@ -24,6 +24,9 @@ int main()
 	string city; // ditto
 	list<Order>::iterator it_orders; //iterator to traverse orders list
 	list<Truck>::iterator it_trucks; // likewise for the truck list
+	int truck_id = 1; //keeps track of truck id# 
+	bool truck_not_found = true; //used to check if a truck was found
+
 	fin.open("refrigerators.txt");
 	
 	fin >> id >> city >> num; //get data from txt file
@@ -39,32 +42,22 @@ int main()
 	}
 	fin.close();
 
-	int truck_id = 1; // starting from zero would be kinda weird (truck zero would be interesting)
-	bool truck_not_found = true; //hmmmm wonder what this could be?
 	for(it_orders = Orders.begin(); it_orders != Orders.end(); it_orders++) {//go through orders one at a time
-		for (it_trucks = Trucks.begin(); it_trucks != Trucks.end(); ++it_trucks)//try to find an existing truck
-		{
+		for (it_trucks = Trucks.begin(); it_trucks != Trucks.end(); ++it_trucks) {//try to find an existing truck
 			if (it_trucks->getDestination() == it_orders->getDestination()) { //if a truck exists
-				try
-				{
+				try{
 					it_trucks->addOrder(*it_orders);//add order to truck
-					truck_not_found = false; //oh I got it now!
-					if (it_trucks->getNumRefrigerators() == it_trucks->getMaxRefrigerators())
-					{ //print out all the details of the truck thats leaving.
-						cout << endl << "Truck # " << it_trucks->getTruckID() << " left for " << it_trucks->getDestination();
-						cout << " with " << it_trucks->getNumRefrigerators() << " refrigerator(s)" << endl;
-						for (int i = 0; i < it_trucks->getContents().size(); i++) {
-							cout << "Order: " << it_trucks->getContents()[i].getOrderID();
-							cout << "  # of Refrigerators: " << it_trucks->getContents()[i].getRefrigerators() << endl;
-						}
+					truck_not_found = false;
+					if (it_trucks->getNumRefrigerators() == it_trucks->getMaxRefrigerators()) {
+					 //print out all the details of the truck thats leaving.
+						displayContents(it_trucks); //see function at end of main()
 						Trucks.erase(it_trucks--); //remove the truck that just left from the list
-
 					}
-					break;
+					break; //found what we needed so look at next order
 				}
 				catch (Order o) //else make a new truck and add the order to it
 				{
-					continue;
+					continue; //keep looking for a truck
 				}
 			}
 		}
@@ -76,28 +69,33 @@ int main()
 			it_trucks->addOrder(*it_orders); //add the order to the new truck
 			if (it_trucks->getNumRefrigerators() == it_trucks->getMaxRefrigerators())
 			{ //this prints if an order fills a truck in one go!
-				cout << endl << "Truck # " << it_trucks->getTruckID() << " left for " << it_trucks->getDestination();
-				cout << " with " << it_trucks->getNumRefrigerators() << " refrigerator(s)" << endl;
-				for (int i = 0; i < it_trucks->getContents().size(); i++) {//print all the orders in the truck
-					cout << "Order: " << it_trucks->getContents()[i].getOrderID();
-					cout << "  # of Refrigerators: " << it_trucks->getContents()[i].getRefrigerators() << endl;
-				}
+				displayContents(it_trucks);//see function at end of main()
 				Trucks.erase(it_trucks--); //remove the truck that just left from the list
 			}
 		}
 		truck_not_found = true; //reset for next iteration
 	}
+	
 	//all trucks have orders, now to send them out
 	it_trucks = Trucks.begin();
 	while (it_trucks != Trucks.end())
 	{
-		cout << endl << "Truck # " << it_trucks->getTruckID() << " left for " << it_trucks->getDestination();
-		cout << " with " << it_trucks->getNumRefrigerators() << " refrigerator(s)" << endl << endl;
-		for (int i = 0; i < it_trucks->getContents().size(); i++) {
-			cout << "Order: " << it_trucks->getContents()[i].getOrderID();
-			cout << "  # of Refrigerators: " << it_trucks->getContents()[i].getRefrigerators() << endl;
-		}
+		displayContents(it_trucks);//see function at end of main()
 		Trucks.erase(it_trucks++); //remove the truck that just left from the list
-	}// hurray I'm done! I deserve a raise don't you think?
+	}
+	cout << endl << endl << "Code written by Scott Edgerly" << endl << endl;
 	return 0;
+}
+
+
+void displayContents(list<Truck>::iterator it)
+//POST: display contents of a Truck
+{
+	cout << endl << "Truck # " << it->getTruckID() << " left for " << it->getDestination();
+	cout << " with " << it->getNumRefrigerators() << " refrigerator(s)" << endl;
+	for (int i = 0; i < it->getContents().size(); i++) {
+		cout << "Order: " << it->getContents()[i].getOrderID();
+		cout << "  # of Refrigerators: " << it->getContents()[i].getRefrigerators() << endl;
+	}
+	return;
 }
